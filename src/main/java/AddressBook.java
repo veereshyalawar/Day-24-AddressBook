b2abd17ef535fe9acc06490553fc6a8a3c88cd01
veeresh <veereshyalawar2@gmail.com>
2022-04-07 23:10:54 +0530

    added uc3 file which can edit contact

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -8,13 +8,13 @@ public class AddressBook {
 	 * Creating a List of ContactDetails of type String using ArrayList here Adding
 	 * new elements in the List
 	 */
-	static List<ContactDetails> contactDetailsList = new ArrayList();
+	static List<ContactDetails> contactDetailsList = new ArrayList<ContactDetails>();
+	static Scanner scanner = new Scanner(System.in);
 
 	/**
 	 * created method addNewContact() to create a new contacts to the AddressBook
 	 */
-	public static void addNewContact() {
-		Scanner scanner = new Scanner(System.in);
+	public void addContact() {
 		System.out.println("Enter First Name : ");
 		String firstName = scanner.next();
 		System.out.println("Enter Last Name : ");
@@ -31,7 +31,6 @@ public class AddressBook {
 		long mobileNumber = scanner.nextLong();
 		System.out.println("Enter your EmailId  : ");
 		String emailId = scanner.next();
-		scanner.close();
 
 		person = new ContactDetails(firstName, lastName, address, city, state, zipCode, mobileNumber, emailId);
 		contactDetailsList.add(person);
@@ -41,6 +40,22 @@ public class AddressBook {
 
 	/**
 	 * created method printContact() to display the data
+	 */
+	public void editContact() {
+		System.out.println("Enter the first name of person to edit Contact : ");
+		String firstName = scanner.next();
+		if (firstName.equalsIgnoreCase(person.getFirstName())) {
+			addContact();
+		} else {
+			System.out.println("The Entered First Name Is Not Match");
+			editContact();
+		}
+	}
+
+	/**
+	 * Main method for manipulation AddressBookCollection
+	 * 
+	 * @param args - Default Java param (Not used)
 	 */
 	public static void printContact() {
 		for (int i = 0; i < contactDetailsList.size(); i++) {
@@ -52,18 +67,6 @@ public class AddressBook {
 					+ "MobileNumber  : " + person.getMobileNumber() + "\n" + "EmailId       : " + person.getEmailId()
 					+ "\n");
 		}
-	}
-
-	/**
-	 * Main method for manipulation AddressBookCollection
-	 * 
-	 * @param args - Default Java param (Not used)
-	 */
-	public static void main(String[] args) {
-		/**
-		 * calling method addNewContact()
-		 */
-		addNewContact();
 	}
 
 }
