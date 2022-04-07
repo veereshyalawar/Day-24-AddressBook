21035050d4aa0262961d2e7410091b1b560ef6ad
veeresh <veereshyalawar2@gmail.com>
2022-04-07 23:20:45 +0530

    added uc6 file which can add multiple AddressBook

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -47,7 +47,7 @@ public class AddressBook {
 		String emailId = scanner.next();
 		person = new ContactDetails(firstName, lastName, address, city, state, zipCode, mobileNumber, emailId);
 		contactDetailsList.add(person);
-		printContact();
+
 	}
 
 	/**
@@ -91,27 +91,10 @@ public class AddressBook {
 			contactDetailsList.remove(i);
 			System.out.println("Contact Deleted");
 			System.out.println("Remaining contacts in the book isgit");
-			printContact();
 		} else {
 			System.out.println("Contact not find");
 		}
 
 	}
 
-	/**
-	 * created method printContact() to display all the contacts
-	 */
-	public void printContact() {
-		for (int i = 0; i < contactDetailsList.size(); i++) {
-			System.out.println("Contact Details");
-			System.out.println("Name         : " + contactDetailsList.get(i).getFirstName() + " "
-					+ contactDetailsList.get(i).getLastName() + "\n" + "Address      : "
-					+ contactDetailsList.get(i).getAddress() + "\n" + "City         : "
-					+ contactDetailsList.get(i).getCity() + "\n" + "State        : "
-					+ contactDetailsList.get(i).getState() + "\n" + "ZipCode      : "
-					+ contactDetailsList.get(i).getZipCode() + "\n" + "MobileNumber : "
-					+ contactDetailsList.get(i).getMobileNumber() + "\n" + "EmailId      : "
-					+ contactDetailsList.get(i).getEmailId() + "\n");
-		}
-	}
 }
